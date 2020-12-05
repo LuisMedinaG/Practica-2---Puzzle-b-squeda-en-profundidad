fdb79c8ed2a0af2ad1dcd00321933676a1edfecd
Luis Medina G <humberto.medina@alumno.udg.mx>
2020-12-05 06:13:15 +0000

    Working A Star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import heapq
 import math
 import cv2
 
@@ -8,10 +9,16 @@ class Vertex:
         self.vert_id = vert_id
         self.x = x
         self.y = y
+
         self.neighbors = []
         self.distance = -1
         self.curr_dist = math.inf
         self.previous = None
+
+        self.gscore = math.inf
+        self.fscore = math.inf
+        self.closed = False
+        self.out_openset = True
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
@@ -22,11 +29,15 @@ class Vertex:
     def __repr__(self):
         return f"ID: {self.vert_id} COORD: ({self.x},{self.y})"
 
+    def __lt__(self, b):
+        return self.fscore < b.fscore
+
     def addVertex(self, vertex):
         self.neighbors.append(vertex)
 
     def getDistance(self, other_vertex):
-        return math.sqrt((other_vertex.x - self.x)**2 + (other_vertex.y - self.y)**2)
+        return math.sqrt((other_vertex.x - self.x)**2 +
+                         (other_vertex.y - self.y)**2)
 
     def getPos(self):
         return (self.x, self.y)
@@ -65,7 +76,7 @@ class ShapeDetector:
                 nodo = Vertex(shp_id, x, y)
                 self.vertex_list.append(nodo)
                 shp_id += 1
-                
+
                 if shp_idx < 2:
                     break
 
@@ -82,7 +93,7 @@ class ShapeDetector:
         pt_b = np.array([vertex2.x, vertex2.y])
         dist = int(vertex1.getDistance(vertex2))
         line = np.linspace(pt_a, pt_b, int(dist / 2), dtype="int")
-        
+
         for i, point in enumerate(line):
             x = int(point[0])
             y = int(point[1])
@@ -95,7 +106,7 @@ class ShapeDetector:
             # Other way of checking the skip radius
             # if vertex1.getDistance(x, y) <= r or vertex2.getDistance(x, y) <= r:
             #     continue
-                
+
             if self.isBlack(r, g, b):
                 return False
         return True
@@ -105,25 +116,30 @@ class ShapeDetector:
             org = self.vertex_list[vert_id]
 
             if drawId:
-                cv2.putText(self.img, str(org.vert_id), org.getPos(), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 169), 2)
+                cv2.putText(self.img, str(org.vert_id), org.getPos(),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 169), 2)
             if drawVertex:
-                cv2.circle(self.img, org.getPos(), 2, (0, 255, 255), thickness=2)
+                cv2.circle(
+                    self.img, org.getPos(), 2, (0, 255, 255), thickness=2)
             if i == len(path) - 1:
                 break
-                
+
             des = self.vertex_list[path[i + 1]]
 
             midX = int((org.x + des.x) / 2)
             midY = int((org.y + des.y) / 2)
             dist = str(int(org.getDistance(des)))
-        
-            cv2.line(self.img, org.getPos(), des.getPos(), (0, 255, 0), thickness=1)
-            
+
+            cv2.line(
+                self.img, org.getPos(), des.getPos(), (0, 255, 0), thickness=1)
+
             if drawDist:
-                cv2.putText(self.img, dist, (midX, midY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (184, 84, 39), 2)
+                cv2.putText(self.img, dist, (midX, midY),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (184, 84, 39), 2)
 
     def isBlack(self, r, g, b):
         return r < 150 and g < 150 and b < 150
+
 
 def shortestPathBFS(start):
     """
@@ -140,10 +156,74 @@ def shortestPathBFS(start):
         curr = queue.pop()
         for neighbor in curr.neighbors:
             next_distance = curr.curr_dist + curr.getDistance(neighbor)
-            if neighbor.curr_dist == math.inf or  neighbor.curr_dist > next_distance:
+            if neighbor.curr_dist == math.inf or neighbor.curr_dist > next_distance:
                 neighbor.curr_dist = next_distance
                 neighbor.previous = curr
-                queue.insert(0, neighbor)        
+                queue.insert(0, neighbor)
+
+
+def heuristic(cell, goal):
+    """computes the 'direct' distance between two (x,y) tuples"""
+    return math.hypot(goal.x - cell.x, goal.y - cell.y)
+
+
+def AStar(start, goal):
+    if start == goal:
+        return
+
+    start.fscore = heuristic(start, goal)
+    start.gscore = 0
+
+    open_set = []
+    heapq.heappush(open_set, start)
+
+    while open_set:
+        current = heapq.heappop(open_set)
+        if current == goal:
+            return
+
+        current.out_openset = True
+        current.closed = True
+
+        for neighbor in current.neighbors:
+            if neighbor.closed:
+                continue
+            
+            tentative_gscore = current.gscore + current.getDistance(neighbor)
+            if tentative_gscore >= neighbor.gscore:
+                continue
+
+            neighbor.previous = current
+            neighbor.gscore = tentative_gscore
+            neighbor.fscore = tentative_gscore + heuristic(neighbor, goal)
+
+            if neighbor.out_openset:
+                neighbor.out_openset = False
+                heapq.heappush(open_set, neighbor)
+            else:
+                open_set.remove(neighbor)
+                heapq.heappush(open_set, neighbor)
+
+    print("No se encontro camino")
+
+
+def bestFirst(start):
+    if start is None:
+        return None
+        
+    # keep track of nodes to be checked
+    queue = [start]
+    start.curr_dist = 0
+    while queue:
+        curr = queue.pop()
+        for neighbor in curr.neighbors:
+            next_distance = curr.curr_dist + curr.getDistance(neighbor)
+            if neighbor.curr_dist == math.inf or neighbor.curr_dist > next_distance:
+                neighbor.curr_dist = next_distance
+                neighbor.previous = curr
+                queue.insert(0, neighbor)
+        queue = (sorted(queue, key=lambda x: x.curr_dist, reverse=True))
+
 
 def traverseShortestPath(target):
     vertexes_in_path = []
@@ -153,6 +233,7 @@ def traverseShortestPath(target):
         target = target.previous
 
     return vertexes_in_path
+
 
 def main():
     image_filename = 'aima_maze_modified.png'
@@ -173,7 +254,12 @@ def main():
     source = sD.vertex_list[0]
     target = sD.vertex_list[1]
 
-    shortestPathBFS(source)
+    print(source)
+    print(target)
+    # print(sD.vertex_list)
+    
+    # bestFirst(source)
+    AStar(source, target)
     vertexes_in_path = traverseShortestPath(target)
     vertexes_in_path.append(0)
     sD.drawLines(vertexes_in_path, True, True)
@@ -183,10 +269,11 @@ def main():
     print('shortest path vertexs IDs: ', vertexes_in_path[::-1])
 
     # Save the image
-    # cv2.imwrite('bfs.png', sD.img)
+    cv2.imwrite('bfs.png', sD.img)
     cv2.imshow(image_filename, sD.img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
